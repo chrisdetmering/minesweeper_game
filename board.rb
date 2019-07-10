ddf3fd0444357b868fca935035bb8b9ee29178fe
froggy <chrisdetmering@gmail.com>
2019-07-10 12:59:53 -0700

    Cleaned up board initialize

--- board.rb
+++ board.rb
@@ -2,8 +2,19 @@ require_relative 'tile'
 
 
 class Board 
-@board = Array.new(9) {Array.new(9)}
+    
+    
+    def self.empty_grid
+        Array.new(9) {Array.new(9)}
+    end 
 
+    def self.add_bombs 
+    end 
+
+
+    def initialize(grid = self.empty_grid
+    @gid = grid
+    end 
 
 
 
